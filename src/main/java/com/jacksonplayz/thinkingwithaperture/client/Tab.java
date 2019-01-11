3a33bf394e24ab699b9eae7061ddf3fb264b4a6a
Ocelot5836 <FinntheRaider@gmail.com>
2019-01-10 19:42:30 -0800

    added animated creative tab

--- src/main/java/com/jacksonplayz/thinkingwithaperture/client/Tab.java
+++ src/main/java/com/jacksonplayz/thinkingwithaperture/client/Tab.java
@@ -1,18 +1,40 @@
 package com.jacksonplayz.thinkingwithaperture.client;
 
-import com.jacksonplayz.thinkingwithaperture.init.ModItems;
+import com.jacksonplayz.thinkingwithaperture.ThinkingWithAperture;
+
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 
 public class Tab extends CreativeTabs {
 
+    private ResourceLocation[] frames;
+    private long lastTime;
+    private int frame;
+
     public Tab() {
         super("thinkingwithaperture");
-        this.setBackgroundImageName("aperture.png");
+        this.frames = new ResourceLocation[8];
+        for (int i = 0; i < this.frames.length; i++) {
+            this.frames[i] = new ResourceLocation(ThinkingWithAperture.MODID, "textures/gui/container/creative_inventory/tab_aperture" + i + ".png");
+        }
+        this.lastTime = System.currentTimeMillis();
+        this.frame = 0;
     }
 
     @Override
     public ItemStack getTabIconItem() {
         return ItemStack.EMPTY;
+    }
+
+    @Override
+    public ResourceLocation getBackgroundImage() {
+        if (System.currentTimeMillis() - this.lastTime >= 100) {
+            this.lastTime = System.currentTimeMillis();
+            this.frame++;
+            if (this.frame >= this.frames.length)
+                this.frame = 0;
+        }
+        return this.frames[this.frame];
     }
 }
