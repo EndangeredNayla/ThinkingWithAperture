3060b7c45cbf0a55c62bcb6034eb0e0bf4a70e67
Ocelot5836 <FinntheRaider@gmail.com>
2019-01-10 19:47:48 -0800

    fixed incorrect creative tab

--- src/main/java/com/jacksonplayz/thinkingwithaperture/items/ItemModRecord.java
+++ src/main/java/com/jacksonplayz/thinkingwithaperture/items/ItemModRecord.java
@@ -7,7 +7,8 @@ public class ItemModRecord extends net.minecraft.item.ItemRecord {
 
     public ItemModRecord(String recordName, SoundEvent sound) {
         super(recordName, sound);
-        this.setRegistryName(ThinkingWithAperture.MODID, "record_" + recordName);
+        this.setRegistryName("record_" + recordName);
         this.setUnlocalizedName("record");
+        this.setCreativeTab(ThinkingWithAperture.TAB);
     }
 }
