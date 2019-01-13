db17a6a5212868108259b60e55fe94cd0af62f0a
JacksonPlayz <jackson.hardaway@gmail.com>
2019-01-13 03:03:54 -0800

    Added companion cube

--- src/main/java/com/jacksonplayz/thinkingwithaperture/entity/EntityCube.java
+++ src/main/java/com/jacksonplayz/thinkingwithaperture/entity/EntityCube.java
@@ -1,28 +1,37 @@
 package com.jacksonplayz.thinkingwithaperture.entity;
 
 import com.jacksonplayz.thinkingwithaperture.init.ModItems;
-
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
-public class EntityCube extends EntityLiving {
+public class EntityCube extends EntityLiving
+{
+    public static final DataParameter<Integer> TYPE = EntityDataManager.<Integer>createKey(EntityCube.class, DataSerializers.VARINT);
 
-    public EntityCube(World worldIn) {
-        super(worldIn);
+    public EntityCube(World world)
+    {
+        super(world);
         this.setSize(1.0F, 1.0F);
     }
 
     @Override
-    protected void initEntityAI() {
+    protected void entityInit()
+    {
+        super.entityInit();
+        this.dataManager.register(TYPE, 0);
     }
 
     @Override
@@ -52,11 +61,54 @@ public class EntityCube extends EntityLiving {
     }
 
     private void dropBlock() {
-        Block.spawnAsEntity(this.world, this.getPosition(), new ItemStack(ModItems.WEIGHTED_CUBE));
+        Block.spawnAsEntity(this.world, this.getPosition(), new ItemStack(ModItems.WEIGHTED_CUBE, 1, this.dataManager.get(TYPE)));
     }
 
     @Override
-    public ItemStack getPickedResult(RayTraceResult target) {
-        return new ItemStack(ModItems.WEIGHTED_CUBE);
+    public ItemStack getPickedResult(RayTraceResult target)
+    {
+        return new ItemStack(ModItems.WEIGHTED_CUBE, 1, this.dataManager.get(TYPE));
+    }
+
+    // Cube Metadata
+
+    public CubeType getType()
+    {
+        return CubeType.byMetadata(this.dataManager.get(TYPE));
+    }
+
+    public void setType(CubeType type)
+    {
+        this.dataManager.set(TYPE, type.getMetadata());
+    }
+
+    public enum CubeType implements IStringSerializable
+    {
+        STORAGE("storage"), COMPANION("companion");
+
+        private String name;
+
+        private CubeType(String name)
+        {
+            this.name = name;
+        }
+
+        @Override
+        public String getName()
+        {
+            return name;
+        }
+
+        public int getMetadata()
+        {
+            return this.ordinal();
+        }
+
+        public static CubeType byMetadata(int metadata)
+        {
+            if (metadata < 0 || metadata >= values().length)
+                metadata = 0;
+            return values()[metadata];
+        }
     }
 }
