7e137f7a016fe85200a31cea7cc2c857f463f564
Ocelot5836 <FinntheRaider@gmail.com>
2019-01-12 22:06:55 -0800

    merge fat turret render into turret render

--- src/main/java/com/jacksonplayz/thinkingwithaperture/client/render/entity/RenderTurret.java
+++ src/main/java/com/jacksonplayz/thinkingwithaperture/client/render/entity/RenderTurret.java
@@ -1,44 +1,70 @@
 package com.jacksonplayz.thinkingwithaperture.client.render.entity;
 
+import javax.annotation.Nullable;
+
 import com.jacksonplayz.thinkingwithaperture.ThinkingWithAperture;
 import com.jacksonplayz.thinkingwithaperture.client.model.entity.ModelTurret;
 import com.jacksonplayz.thinkingwithaperture.entity.EntityTurret;
+
+import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import javax.annotation.Nullable;
-
 @SideOnly(Side.CLIENT)
-public class RenderTurret extends RenderLiving<EntityTurret> {
-
+public class RenderTurret extends RenderLiving<EntityTurret>
+{
     public static final ResourceLocation NORMAL = new ResourceLocation(ThinkingWithAperture.MODID, "textures/entity/turret/turret.png");
+    public static final ResourceLocation FAT = new ResourceLocation(ThinkingWithAperture.MODID, "textures/entity/turret/big_turret.png");
 
-    // public static final ModelTurret model = new ModelTurret();
+    public static final ModelTurret NORMAL_MODEL = new ModelTurret();
+    public static final ModelTurret FAT_MODEL = new ModelTurret();
 
-    public RenderTurret(RenderManager renderManager) {
-        super(renderManager, new ModelTurret(), 0.5F);
+    public RenderTurret(RenderManager renderManager)
+    {
+        super(renderManager, NORMAL_MODEL, 0.0f);
     }
 
-
-
     @Override
-    public void doRender(EntityTurret entity, double x, double y, double z, float entityYaw, float partialTicks) {
-        super.doRender(entity, x, y, z, entityYaw, partialTicks);
-        super.bindEntityTexture(entity);
-       /* GlStateManager.pushMatrix();
-        GlStateManager.translate(x, y - 0.5, z);
-        GlStateManager.scale(0.0625, 0.0625, 0.0625);
-        model.render(entity, 0.0F, 0.0F, 0.0F,0.0F, 0.0F,1.0F);
-        GlStateManager.popMatrix(); */
-
+    protected void preRenderCallback(EntityTurret entity, float partialTicks)
+    {
+        this.mainModel = getModel(entity);
+        this.shadowSize = entity.getType().getShadowSize();
     }
 
     @Nullable
     @Override
-    protected ResourceLocation getEntityTexture(EntityTurret entity) {
+    protected ResourceLocation getEntityTexture(EntityTurret entity)
+    {
+        switch (entity.getType())
+        {
+        case DEFECTIVE:
+            return NORMAL;
+        case FAT:
+            return FAT;
+        case NORMAL:
+            break;
+        case ORACLE:
+            break;
+        }
         return NORMAL;
+    }
+
+    private static <T extends ModelBase> T getModel(EntityTurret entity)
+    {
+        switch (entity.getType())
+        {
+        case NORMAL:
+            return (T) NORMAL_MODEL;
+        case FAT:
+            return (T) FAT_MODEL;
+        case DEFECTIVE:
+            break;
+        case ORACLE:
+            break;
+        }
+        return (T) NORMAL_MODEL;
     }
 }
